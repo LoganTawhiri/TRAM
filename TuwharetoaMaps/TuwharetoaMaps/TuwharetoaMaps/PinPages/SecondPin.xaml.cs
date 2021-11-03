8a8c84ad1066ff8f243964e9a143e5d8fea3c0e8
30006830 <30006830@student.toiohomai.ac.nz>
2021-11-03 20:43:23 +1300

    Pin pages added

---
+++ TuwharetoaMaps/TuwharetoaMaps/TuwharetoaMaps/PinPages/SecondPin.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TuwharetoaMaps.Pages;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TuwharetoaMaps.PinPages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SecondPin : ContentPage
+    {
+        public SecondPin()
+        {
+            InitializeComponent();
+        }
+
+        private async void BacktoMap_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Map());
+        }
+    }
+}
---
+++ TuwharetoaMaps/TuwharetoaMaps/TuwharetoaMaps/PinPages/ThirdPin.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TuwharetoaMaps.Pages;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TuwharetoaMaps.PinPages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ThirdPin : ContentPage
+    {
+        public ThirdPin()
+        {
+            InitializeComponent();
+        }
+
+        private async void BacktoMap_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Map());
+        }
+    }
+}
